fc87bbe226a705d791dce6bbd796430d677592b1
Aleksandar <apetrovic321@gmail.com>
2023-10-18 04:01:41 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rock-paper-scissors
+
+Playing around with the Odin Project
+Rock Paper Scissors refresher, following instructions (mostly)
---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Play
+let computerSelection = getComputerChoice();
+let playerSelection = "rock"; // play here!
+
+console.log(computerSelection); // display computerSelection
+console.log(`Result: ${playRound(computerSelection, playerSelection)}`); // display result
+
+// computerChoice
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  let computerChoice;
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  } else if (randomNumber === 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
+}
+
+// playRound
+function playRound(computerSelection, playerSelection) {
+  let result;
+
+  if (computerSelection === playerSelection) {
+    result = "Tie Game!"; // tie game scenario
+  }
+  // computer wins scenario
+  else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissors" && playerSelection === "paper")
+  ) {
+    computerSelection = computerSelection
+      .toUpperCase()
+      .charAt(0)
+      .concat(computerSelection.slice(1)); // capitalize the first letter of the second sentence
+    result = `You Lose! ${computerSelection} beats ${playerSelection}!`;
+  }
+  // player wins scenario
+  else if (
+    (computerSelection === "rock" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "rock")
+  ) {
+    playerSelection = playerSelection
+      .toUpperCase()
+      .charAt(0)
+      .concat(playerSelection.slice(1)); // capitalize the first letter of the second sentence
+    result = `You Win! ${playerSelection} beats ${computerSelection}!`;
+  }
+
+  return result;
+}
